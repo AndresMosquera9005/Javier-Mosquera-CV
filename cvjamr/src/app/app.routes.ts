fe5dd6a07072a49be952ffeda9e5e5e31ae2508e
AndresMosquera9005 <andrubjj@gmail.com>
2026-02-28 23:01:48 -0500

    se agrega app-cpmponent.html

--- cvjamr/src/app/app.routes.ts
+++ cvjamr/src/app/app.routes.ts
@@ -1,11 +1,12 @@
 import { Routes } from '@angular/router';
 import { Proyecto } from './components/proyecto/proyecto';
-import { Inicio } from './components/inicio/inicio';
-// import { ProyectoComponent } from './components/proyecto/proyecto.component';
 
 export const routes: Routes = [
-    { path: 'inicio', component: Inicio },
-    { path: 'proyecto', component: Proyecto },
-    { path: '', redirectTo: '/inicio', pathMatch: 'full' }
+    {
+        path: 'proyecto', 
+        title: 'proyecto',
+        component: Proyecto,
+        loadComponent: () => import('./components/proyecto/proyecto').then(m => m.Proyecto)
+    },
     
 ];
